d320fe03ceaef04a69e31fd8dc94ddecaa082ad5
sarah loghmani <sarah.loghmani80@gmail.com>
2022-01-15 16:22:04 +0330

    4

---
+++ folder5-15jan2022/reding.js
@@ -0,0 +1,27 @@
+// objects
+// *declarative (literal) form:
+const obj = {
+  key1: 'valu1',
+  key2: 'valu2',
+  key3: 'valu3',
+  key4: 'valu4',
+};
+console.log(obj);
+// *constructed form:
+const obj1 = new Object()
+obj1.key1 = 'valu1'
+obj1.key2 = 'valu2'
+obj1.key3 = 'valu3'
+obj1.key4 = 'valu4';
+console.log(obj1);
+// language types in JS(6 primary types):
+/*  string
+    number
+    boolean
+    null=> never ever is not object
+    undefined
+    object
+*/
+// !"everything in JavaScript is NOT an object"
+
+
