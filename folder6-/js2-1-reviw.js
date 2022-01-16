83adcc9f544f4846fa83b889166989a1a1846d42
sarah loghmani <sarah.loghmani80@gmail.com>
2022-01-16 16:01:34 +0330

    object.keys

--- folder6-/js2-1-reviw.js
+++ folder6-/js2-1-reviw.js
@@ -1,4 +1,4 @@
-// for in
+// for(variable in obj) {}
 const obj = {
     a:1,
     b:2,
@@ -8,3 +8,31 @@ const obj = {
 for(let key in obj){
     console.log(`${key.toLocaleUpperCase()} : ${obj[key]}`);
 }
+// -----------------------------------------------------------------------
+// object.keys(obj)
+
+const students = {
+    sarah: 19,
+    Aslan: 15,
+    jan: 18,
+    ABASS: 20,
+    FERESHTE: 19,
+}
+let arr = Object.keys(students).map((ele) => ele.toUpperCase());
+console.log(arr);//[ 'SARAH', 'ASLAN', 'JAN', 'ABASS', 'FERESHTE' ]
+for (let key in students){
+  console.log(key.toUpperCase());
+  /*SARAH
+    ASLAN;
+    JAN;
+    ABASS;
+    FERESHTE;
+  */
+}
+
+const flowers ={ 
+    rose:'red', 
+    lili:'purple', 
+    liliium:'White'
+}
+console.log(Object.keys(flowers));
