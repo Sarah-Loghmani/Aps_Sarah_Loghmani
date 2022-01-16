f3120560f90c2b22fed37d386f5416c9a4b5d4d6
sarah loghmani <sarah.loghmani80@gmail.com>
2022-01-16 21:13:17 +0330

    this ended

--- folder6-/js2-1-reviw.js
+++ folder6-/js2-1-reviw.js
@@ -48,38 +48,82 @@ function funcName(){
 }
 // execute / invoke
 funcName()
+
 // nested function
-function funcName2() {
+const funcName2 = function() {
+    const nested = ()=>
   console.log(this); //window
+  nested()
 }
 funcName2();
+
 // arrow function
-const funcName3 = ()=> console.log(this);
+const funcName3 = ()=> console.log(this);//window
 funcName3()
 
 // ? method -> object
 // method  =  a function in an object
 const test = {
-    firstName : 'sara',
-    lastName : 'loghmani',
-    age : 33,
-    fullName(){
-        console.log(this);//object(local scope)
-        return `${this.firstName.toUpperCase()} ${this.lastName.toUpperCase()}`; 
+  firstName: "sara",
+  lastName: "loghmani",
+  age: 33,
+// decalaration function
+  fullName() {
+    console.log(this); //object(local scope)
+    return `${this.firstName.toUpperCase()} ${this.lastName.toUpperCase()}`;
+  },
+
+  greeting: function () {
+    console.log(this); //object(local scope)
+    return `'Hello dear ${this.fullName()}`;
+  },
+  
+  // decalaration function nested in decalaration function
+  func1() {
+      console.log(this); //object
+      const nested = function () {
+          console.log(this); //window
+        };
+        nested();
+        return "<3";
     },
-    greeting : function(){
-        console.log(this);//object(local scope)
-        return `'Hello dear ${this.fullName()}`
-    },
-    birthDay : ()=>{
-        console.log(this);//window(global scope)
+    //   arrow function nested in decalaration function
+      func2() {
+        console.log(this); //object
+        const nested =  () =>{
+          console.log(this); //object
+        };
+        nested();
+        return "<3";
+      },
+
+    // arrow function
+      birthDay: () => {
+        console.log(this); //window(global scope)
         // return `'your birthyear is ${1400 - this.age}`// undefined
         return `'your birthyear is ${1400 - test.age}`;
+      },
+//  decalaration function nested in arrow function
+    arrow: ()=>{
+        console.log(this);//window
+        function nested(){
+            console.log(this);//window
+        }
+        nested()
     },
-    func1(){
-        
+    // arrow func nested in arrow
+    arrow2: ()=>{
+        console.log(this);//window
+         const nested=()=>{
+            console.log(this);//window
+        }
+        nested()
     }
-}
+};
 console.log(test.fullName());
 console.log(test.greeting());
+console.log(test.func1());
+test.func2()
 console.log(test.birthDay());
+test.arrow()
+test.arrow2()
