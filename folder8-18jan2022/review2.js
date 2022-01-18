3ee197acf5b0adaec245137467a76305612fd290
sarah loghmani <sarah.loghmani80@gmail.com>
2022-01-18 14:08:23 +0330

    3

---
+++ folder8-18jan2022/review2.js
@@ -0,0 +1,56 @@
+// *block 
+// function x() {
+//   let num = 6;
+// }
+// x();
+// console.log(num)//is not definde
+// // console.log(x()); //undefind
+// ----------------------------
+
+// const a = 0
+// function y(){
+//     a=6
+// }
+// // console.log(a);
+// y()
+// ------------------------------
+
+// let s = 1
+// if (s>0){
+//     g = 1
+//     // var g = 1
+//     // let g = 1
+//     // const g = 1;
+// }
+// console.log(g);
+
+//* 
+// let arr=[1,2,3,4,5,6,7]
+// let arr2 =[]
+
+// for(let el of arr){
+//    console.log(arr2.push(el * 2));
+// }
+// console.log(arr2);
+// 
+// *
+// let arr3 =[]
+// for(let i = 0 ; i < arr.length ; i++){
+//     // let arr2 = [];
+//     arr3.push(arr[i]*2)
+//     // console.log(arr2);
+// }
+// console.log(arr3);
+// -------------------------------------------------
+
+// const number =  2
+// function h(){
+//     const number = 5;
+//     console.log(number);
+// }
+// console.log(number);
+// h()
+// console.log(number);
+// -----------------------------------------------
+
+// 
