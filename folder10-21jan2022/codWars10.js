fed9e35c5e26b9a473cf7b872a28112a6ec30eb3
sarah loghmani <sarah.loghmani80@gmail.com>
2022-01-22 15:38:18 +0330

    1

--- folder10-21jan2022/codWars10.js
+++ folder10-21jan2022/codWars10.js
@@ -1,5 +1,3 @@
-
-
 // Given an array of integers, find the one that appears an odd number of times.
 
 // There will always be only one integer that appears an odd number of times.
@@ -10,16 +8,22 @@
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+let array = [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1];
+console.log(array);
+console.log('hi');
 
-const findOdd = (arr)=>{
-    
-    for(let i = 0 ; i<arr.length ; i++){
-        for(let j = 0 ; j<arr.length ; i++){
-            if(i === j){
-                count++
-            }
-        }
+const findOdd = (arr) => {
+  let count = 0;
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; i++) {
+      if (arr[i] === arr[j]) {
+        count++
+        obj[arr[i]] = count
+        console.log(obj);
+      }
     }
-    return
-
-}
+  }
+  // return
+};
+findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]);
