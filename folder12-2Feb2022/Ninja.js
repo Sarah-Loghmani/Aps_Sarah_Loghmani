493f401b73a6efcc048d893270f22f3b629dc90e
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-02 21:09:50 +0330

    2

--- folder12-2Feb2022/Ninja.js
+++ folder12-2Feb2022/Ninja.js
@@ -2,12 +2,29 @@
 let pEl = document.querySelectorAll("p");
 console.log(pEl);
 
+let article = document.createElement("article");
+
+let h2El = document.createElement("h2");
+h2El.innerText = "This is a test."
+article.append(h2El)
+document.body.append(article);
+
+let ulEl = document.createElement("ul");
+
 pEl.forEach(p =>{
+
     if(p.textContent.includes("error")){
         p.classList.add("error")
-    }else if(p.textContent.includes("success")){
+    }
+    if(p.textContent.includes("success")){
         p.classList.toggle("success")
     }
+
+    let liEl = document.createElement("li");
+    liEl.append(p);
+    ulEl.append(liEl);
+    article.append(ulEl);
+
 })
 
 let spanClass = document.querySelector(".span");
@@ -15,34 +32,11 @@ let spanClass = document.querySelector(".span");
 // console.log(spanClass.innerHTML);
 // console.log(spanClass.textContent);// all of the content
 
-let article = document.createElement("article");
-
-let h2El = document.createElement("h2");
-h2El.innerText = "This is a test."
-article.append(h2El)
-
-let ulEl = document.createElement("ul");
-// article.append(ulEl)
-
-pEl.forEach(p => {
-    // article.append(p)
-    p.style.cursor = "pointer";
-
-    let liEl = document.createElement("li");
-    liEl.append(p)
-
-    ulEl.append(liEl)
-    article.append(ulEl)
-    document.body.append(ulEl)
-});
-
 // console.log(ulEl.children);
 
 let divEl = document.createElement("div");
 divEl.innerText = "this is a div for test";
 article.append(divEl);
-
-document.body.appendChild(article);
 
 console.log(article.children);//this is a htmlcollection
 
@@ -61,5 +55,40 @@ console.log(spanClass.nextElementSibling);
 console.log(spanClass.previousElementSibling);//null => there is not any previous element
 
 // * events
+let button=document.createElement("button");
+button.innerText = "Click me";
+article.append(button);
 
+button.addEventListener("click", () =>{
+    console.log("you click on me");
+    // console.log(e);
+});
+
+// console.log((ulEl.children));
+(Array.from(ulEl.children)).forEach(li => {
+li.addEventListener("click", e => {
+  // console.log(e);
+  console.log(e.target); //best practice
+  // console.log(p);// not best
+
+  e.target.style.textDecoration = "line-through";
+
+  // *remove elements
+  e.target.remove()
+})
+})
+
+
+button.addEventListener("click", ()=>{
+
+    // ulEl.innerHTML += `<li><p>new list</p></li>`;//this is appended from end by default. and don't follow from event that is added to its sibling elements.
+    // or
+    let p = document.createElement("p");
+    let li = document.createElement("li");
+    p.textContent = "new list2"
+    li.append(p)
+    ulEl.append(li);
+    // To adding the element from first of parrent:
+    // ulEl.prepend(li);
+})
 
