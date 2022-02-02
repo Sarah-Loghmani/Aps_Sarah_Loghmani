f193f0596b02492b39355e11fc9ebaec2dda71e1
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-02 16:37:04 +0330

    5

--- folder12-2Feb2022/Ninja.js
+++ folder12-2Feb2022/Ninja.js
@@ -11,9 +11,9 @@ for(let p of pEl){
 }
 
 let spanClass = document.querySelector(".span");
-console.log(spanClass.innerText);// only visible text
-console.log(spanClass.innerHTML);
-console.log(spanClass.textContent);// all of the content
+// console.log(spanClass.innerText);// only visible text
+// console.log(spanClass.innerHTML);
+// console.log(spanClass.textContent);// all of the content
 
 let article = document.createElement("article");
 
@@ -21,7 +21,22 @@ let h2El = document.createElement("h2");
 h2El.innerText = "This is a test."
 article.append(h2El)
 
-pEl.forEach(p => article.append(p));
+let ulEl = document.createElement("ul");
+// article.append(ulEl)
+
+pEl.forEach(p => {
+    // article.append(p)
+    p.style.cursor = "pointer";
+
+    let liEl = document.createElement("li");
+    liEl.append(p)
+
+    ulEl.append(liEl)
+    article.append(ulEl)
+    document.body.append(ulEl)
+});
+
+// console.log(ulEl.children);
 
 let divEl = document.createElement("div");
 divEl.innerText = "this is a div for test";
@@ -34,8 +49,17 @@ console.log(article.children);//this is a htmlcollection
 //* it is impossible to use foreach for htmlcollection so have to convert it to an array:
 console.log(Array.from(article.children));//this is not distroctive.
 
-
 Array.from(article.children).forEach(child => {
     console.log(child.innerText);
     child.style.color ="brown"
 })
+
+// * To understand the parentElement of an element:
+console.log(spanClass.parentElement);
+console.log(spanClass.parentElement.parentElement);
+console.log(spanClass.nextElementSibling);
+console.log(spanClass.previousElementSibling);//null => there is not any previous element
+
+// * events
+
+
