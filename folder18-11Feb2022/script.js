6e4c86fcc0b743a04a569cfe48137fdd1a4e843f
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-11 16:19:17 +0330

    2

---
+++ folder18-11Feb2022/script.js
@@ -0,0 +1,6 @@
+const request = new XMLHttpRequest()//creat arequest object
+request.open('GET', )//first arqument is the type of our request
+// get=> to get data 
+// post=> to send data to
+//put & patch=> to update data
+//delete=> to delete data
