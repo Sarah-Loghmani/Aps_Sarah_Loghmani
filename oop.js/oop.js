a6cee20da886e3886736c69c7aa32377e1b0467f
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-27 17:09:19 +0330

    4

--- oop.js/oop.js
+++ oop.js/oop.js
@@ -1,22 +1,40 @@
-class User{
-    constructor(name, email, password){
+// class User{
+//     constructor(name, email, password){
         
-            this.name = name;
-            this.email = email;
-            this.password = password;
-    };
-    login(){
+//             this.name = name;
+//             this.email = email;
+//             this.password = password;
+//     };
+//     login(){
+
+//         console.log(this.name + " Logged in");
+//         return this;// for chaining methods
+//     };
+//     logout(){
+         
+//          console.log(this.name + " Logged out");
+//          return this;//for chaining methods
+//     };
+
+
+// }
+
+// *syntax sugar
+
+function User(name, email, password){
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.login=function(){
 
         console.log(this.name + " Logged in");
         return this;// for chaining methods
     };
-    logout(){
+    this.logout = function(){
          
          console.log(this.name + " Logged out");
          return this;//for chaining methods
     };
-
-
 }
 const user1 = new User('Sarah Loghmani', 'sara@mail.com','123456');
 const user2 = new User("Shokat Etemadi", "shokat@mail.com", "123456");
@@ -32,6 +50,10 @@ user2.login().logout();
 // *subclasses
 
 class Admin extends User {
+    constructor(name, email, password, title){
+        super(name, email, password)
+        this.title = title
+    }
 
     deleteUser(user) {
        users= users.filter( u => u.name !== user.name)
@@ -40,12 +62,12 @@ class Admin extends User {
  
 }
 
-const user3 = new User('Rahman Ajory', 'Raman.mail.com')
+const user3 = new User('Rahman Ajory', 'Raman.mail.com', 95484)
 
 let users =[user1,user2, user3];
 console.log(users)
  
-const admin = new Admin('Iran Daroodi', 'Iran.mail.com');
+const admin = new Admin('Iran Daroodi', 'Iran.mail.com', 95484, 'painter');
 
 admin.deleteUser(user2)
 console.log(admin);
