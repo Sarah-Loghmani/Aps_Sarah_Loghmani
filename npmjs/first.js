e192a1fc351ce87ea58aefd609c2dc198bd9def6
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-11 20:16:50 +0330

    3

---
+++ npmjs/first.js
@@ -0,0 +1,6 @@
+//* npm => node package manager(node modules)
+//* We can install or uninstall node modules:[1]=>global=> accessible every where (not recommended) [2]=>local => just access on the local file (recommended)
+//? https://www.npmjs.com/
+//? https://yarnpkg.com/
+
+
