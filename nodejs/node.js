6b8d6806b057950ff2f3dbcf21274ccf36beb929
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-11 13:45:29 +0330

    1

--- nodejs/node.js
+++ nodejs/node.js
@@ -11,22 +11,29 @@ const sum = (a, b) => console.log(a + b);
 module.exports.sum = sum;
 module.exports.multiply = (a, b) => console.log(a * b);
 module.exports.name = "SARAH";
-//* We can add any property to the exports object.Then it is ready to use these properties in another file.  
+//* We can add any property to the exports object.Then it is ready to use these properties in another file.
 
 // console.log(module);
 //*go to app.js
 
-
 // ?  File System
 //?--------------
 //* fs node is an big object that when we require we call it:
-const fs = require('fs');
+const fs = require("fs");
 
 //* there are a lot of method in it that let interact with files of system.
 
 // console.log(fs);
+//* assign folder names in a variable
+const directoryName = {
+  async: "DirectoryAsync",
+  sync: "DirectorySync",
+};
 
-fs.mkdir('sarah', )
-fs.mkdir('sara', { recursive: true })
+//* Create A Directory In NodeJS (Async)
+fs.mkdir(`${directoryName.async}`, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+//* Create A Directory In NodeJS (Sync)
 
-
+fs.mkdirSync(`${directoryName.sync}`, { recursive: true });
