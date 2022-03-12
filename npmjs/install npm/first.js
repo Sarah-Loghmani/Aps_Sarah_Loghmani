67c6f87ccc63cf4730c9db244049d01789cc84a6
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-12 16:30:17 +0330

    1

---
+++ npmjs/install npm/first.js
@@ -0,0 +1,9 @@
+//* first of all i direct the terminal to this local file
+//* then write "npm init -y" commend in the terminal
+//* the package.json is created
+//* i write "npm i bootstrap"
+//* added dependencies property in package.json
+//* i go to index.html
+//* and import the link into the <head> element.
+
+
