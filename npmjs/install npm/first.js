a2fc4ee4141e015ca7983c08f9bdbd9b30bca310
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-12 22:09:35 +0330

    2

--- npmjs/install npm/first.js
+++ npmjs/install npm/first.js
@@ -4,6 +4,21 @@
 //* i write "npm i bootstrap"
 //* added dependencies property in package.json
 //* i go to index.html
-//* and import the link into the <head> element.
+//* and import the link into the <head> element.(<link>for graphical paths such as bootstrap & fontawesome or <script> for js such as axios)
+
+//TODO => to show in terminal
+
+const joke = require("give-me-a-joke");
+const colors = require("colors");
+joke.getRandomDadJoke((joke) => console.log(joke.america));
 
 
+//TODO => let's make a app
+
+const joke = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () =>{
+    axios
+    .get()
+})
