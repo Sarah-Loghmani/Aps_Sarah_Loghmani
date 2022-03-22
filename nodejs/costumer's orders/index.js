b7da4394dceb8d59807fe01a106910e67f5d8709
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-22 23:10:55 +0430

    1

--- nodejs/costumer's orders/index.js
+++
@@ -1,9 +0,0 @@
-const coffee = require("./coffee");
-const cake = require("./cake");
-const pasta = require("./pasta");
-
-const costumerOrder = [coffee, cake, pasta];
-
-console.log(costumerOrder);
-
-module.exports = costumerOrder;
