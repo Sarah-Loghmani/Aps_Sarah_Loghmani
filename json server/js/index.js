2c7e3fd8f5a768acb5096817d6f0605813830fab
sarah loghmani <sarah.loghmani80@gmail.com>
2022-04-02 23:35:06 +0430

    3

---
+++ json server/js/index.js
@@ -0,0 +1,4 @@
+//* json server is a package we install on computer with npm install
+
+//* in the https://www.npmjs.com/ search "json server", scroll down and copy "npm install -g json-server" 
+//* in the command prompt pate it and press enter; it is installed on pc;
